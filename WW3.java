e2ddb28d1cc6a558bfbc5cb6742f0e9015f2b4af
awing-ding <66324442+awing-ding@users.noreply.github.com>
2020-07-28 13:01:40 +0200

    Update WW3.java

--- WW3.java
+++ WW3.java
@@ -31,6 +31,20 @@ class ClasseMain{
     System.out.println("Combien de popularité les MJ/roll vous ont fait perdre ? ");
     int PopulariteArbitraireGagne = sc.nextInt();
     System.out.println("Combien de popularité les MJ/roll vous ont fait gagner ? ");
+    int Popularite = 75+(Propagande+(Musee*2)+(Cinemas*2)+PopulariteArbitraireGagne)-(5*Nucleaire+PopulariteArbitrairePerdu+(population/tauxDePertePopularite));
+    int PIB = exCaisse+(Usine*2)+(Nucleaire*6)+(Ferme*4)+(Port*3)+((recettePublique/1000)*annexion);
+    if Popularite <= 40 : #calcul
+        caisse = PIB-(exCaisse*0.2)
+    else if Popularite > 40 and Popularite <= 60:
+        caisse = PIB-(exCaisse*0.1)
+    else if Popularite > 60 and Popularite <= 80:
+        caisse = PIB+(exCaisse*0.1)
+    else if Popularite > 80 and Popularite <= 100:
+        caisse = PIB+(exCaisse*0.2)
+    if caisse < 10;
+        caisse = 10
+    System.out.println("vos caisse sont de " + caisse)
+    System.out.println("votre popularité est égale à:" + Popularite)
   }//Fin de la méthode main
 
 }//Fin de votre classe
