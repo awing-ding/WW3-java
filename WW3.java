7c340bf79e9043310e0388b608dd3ff48a99a5af
awing-ding <66324442+awing-ding@users.noreply.github.com>
2020-07-28 11:38:05 +0200

    Update WW3.java
    début de code

--- WW3.java
+++ WW3.java
@@ -1,1 +1,36 @@
-public cla
+import java.util.Scanner;
+class ClasseMain{
+
+  public static void main(String[] args){ 
+    Scanner sc = new Scanner(System.in);
+    int exCaisse = sc.nextInt();
+    System.out.println("quels sont vos anciennes caisses ? ");
+    int recettePublique = sc.nextInt();
+    System.out.println("quels sont vos recette publique en % mais sans le <<%>> ? ");
+    int annexion = sc.nextInt();
+    System.out.println("quel est le total du PIB des pays que vous avez annexé ? ");
+    int tauxDePertePopularite = sc.nextInt();
+    System.out.println("par combien votre population est-elle divisé pour calculer votre popularité (si vous ne savez pas, contactez le staff) ? ");
+    int population = sc.nextInt();
+    System.out.println("quel est votre population ? ");
+    int Musee = sc.nextInt();
+    System.out.println("combien de musée avez vous construit ? ");
+    int Propagande = sc.nextInt();
+    System.out.println("combien de propagande avez vous fait ? ");
+    int Cinemas = sc.nextInt();
+    System.out.println("combien de cinéma avez vous construit ? ");
+    int Usine = sc.nextInt();
+    System.out.println("combien d'usine avez vous construit ? ");
+    int Nucleaire = sc.nextInt();
+    System.out.println("combien de centrale nucléaire avez vous construit ? ");
+    int Ferme = sc.nextInt();
+    System.out.println("combien de ferme avez vous construit ? ");
+    int Port = sc.nextInt();
+    System.out.println("combien de port avez vous construit ? ");
+    int PopulariteArbitrairePerdu = sc.nextInt();
+    System.out.println("Combien de popularité les MJ/roll vous ont fait perdre ? ");
+    int PopulariteArbitraireGagne = sc.nextInt();
+    System.out.println("Combien de popularité les MJ/roll vous ont fait gagner ? ");
+  }//Fin de la méthode main
+
+}//Fin de votre classe
